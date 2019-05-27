54c7757a996eabb5bbcba2fb6e121147b1349515
Viktor VAD <viktor@andopen.co>
2019-05-27 16:43:44 +0100

    Adding docker-compose.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -30,3 +30,6 @@ systemctl daemon-reload
 # add docker group and add vagrant to it
 groupadd docker
 usermod -a -G docker vagrant
+
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
